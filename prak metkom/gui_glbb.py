1c0a392ac5fa9f44fc4c62e421e4ef093dbb4f73
rizalfadkam <rizal.fadhilah.kamal@mail.ugm.ac.id>
2022-11-30 11:08:32 +0700

    commit

--- prak metkom/gui_glbb.py
+++ prak metkom/gui_glbb.py
@@ -4,27 +4,27 @@ import tkinter.font
 from modul_glbb import glbb
 
 root = Tk()
-root.geometry('600x600')
+root.geometry('300x300')
 
 changefont = tkinter.font.Font(size=20)
 Title = Label(root, text = "GLBB", font = changefont)
-Title.place(x=250, y=10)
+Title.place(x=115, y=10)
 
 x = Label(root, text = 'Kecepatan Awal')
 y = Label(root, text = 'Akselerasi Awal')
 z = Label(root, text = 'Waktu')
 
-e1 = Entry(root,width=80)
-e2 = Entry(root,width=80)
-e3 = Entry(root,width=80)
+e1 = Entry(root,width=40)
+e2 = Entry(root,width=40)
+e3 = Entry(root,width=40)
 
-x.place(x = 250, y = 50)
-y.place(x = 250, y = 90)
-z.place(x = 250, y = 130)
+x.place(x = 113, y = 50)
+y.place(x = 114, y = 90)
+z.place(x = 133, y = 130)
 
-e1.place(x = 55, y = 70)
-e2.place(x = 55, y = 110)
-e3.place(x = 55, y = 150)
+e1.place(x = 28, y = 70)
+e2.place(x = 28, y = 110)
+e3.place(x = 28, y = 150)
 
 def plot():
     v0 = float(e1.get())
@@ -35,5 +35,5 @@ def plot():
 plot_button = Button(master = root,
                     command=plot,
                     text='Hitung Kecepatan dan Jarak')
-plot_button.place(x = 155, y = 190)
+plot_button.place(x = 70, y = 190)
 root.mainloop()
--- prak metkom/modul_glbb.py
+++ prak metkom/modul_glbb.py
@@ -5,24 +5,29 @@ import threading
 
 def glbb(v0,t,a):
     root = tk.Tk()
-    canvas = tk.Canvas(root, width=500, height=600)
+    canvas = tk.Canvas(root, width=500, height=400)
     canvas.pack()
 
     def semua(v0,t,a):
         tp = turtle.RawTurtle(canvas)
+        sizefont = 18
+        sizef = 15
+        FONT = ('Halvetica', sizefont, 'normal')
+        Font = ('Halvetica', sizef, 'normal')
         tp.penup()
-        tp.goto(-100,-150)
+        tp.pensize(3)
+        tp.goto(-100,0)
         tp.pendown()
         s = v0*t + ((a*(t**2))/2)
         Vt = v0 + a*t
         tp.fillcolor('cyan')
         tp.forward(200)
         tp.penup()
-        tp.write(" "+str(s)+" m")
+        tp.write(" "+str(s)+" m", font=Font)
         tp.penup()
-        tp.goto(-100,-170)
+        tp.goto(-240,50)
         tp.pendown()
-        tp.write("Kecepatan Akhir Sebesar : " + str(Vt) + "  m/s")
+        tp.write("Kecepatan Akhir Sebesar : " + str(Vt) + "  m/s", font=FONT)
         tp.hideturtle()
         
     
